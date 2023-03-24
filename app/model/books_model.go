12fe5721fd5cdf89744877138b8b458116c9a81f
yasu2122yasu <tyasushi@netprotections.co.jp>
2023-03-24 12:00:43 +0900

    挑戦者が使うブランチの作成

--- app/model/books_model.go
+++ app/model/books_model.go
@@ -1,8 +1,6 @@
 package model
 
 import (
-	"fmt"
-
 	"gorm.io/gorm"
 )
 
@@ -18,34 +16,4 @@ func GetAll() (books []Book) {
 		panic(result.Error)
 	}
 	return
-}
-
-func GetOne(id int) (book Book) {
-	result := Db.First(&book, id)
-	if result.Error != nil {
-		panic(result.Error)
-	}
-	return
-}
-
-func (b *Book) Create() {
-	result := Db.Create(b)
-	if result.Error != nil {
-		panic(result.Error)
-	}
-}
-
-func (b *Book) Update() {
-	result := Db.Save(b)
-	if result.Error != nil {
-		panic(result.Error)
-	}
-}
-
-func (b *Book) Delete() {
-	result := Db.Delete(b)
-	fmt.Println(result)
-	if result.Error != nil {
-		panic(result.Error)
-	}
 }
